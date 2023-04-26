5b9d9b8ae8207abeef92eeb26602733a580aac17
Victoria <dmitrieva7v@gmail.com>
2023-04-27 00:56:39 +0300

    Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int remain = num % 2;
+if (remain == 0)
+{
+    Console.WriteLine($"Число {num} является четным");
+}
+else 
+{
+    Console.WriteLine($"Число {num} является нечетным");
+}
