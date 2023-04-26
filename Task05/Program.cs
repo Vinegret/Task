bb5c8ed678d94afeac176e03bd99d77a86f7ad08
Victoria <dmitrieva7v@gmail.com>
2023-04-27 00:01:26 +0300

    Task05

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числa в промежутке от -N до N
+
+Console.WriteLine("Введите целое положительное число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = -num1;
+while (num2 <= num1)
+{
+    Console.Write($" {num2} ,");
+    num2 ++;
+}
