c3443a7239c092a25d33a9c566f07f74be401ddb
Victoria <dmitrieva7v@gmail.com>
2023-04-27 01:08:24 +0300

    Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите целое положительное число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = 2;
+while (num2 <= num1)
+{
+    Console.Write($" {num2} ,");
+    num2 = num2 + 2;
+}
