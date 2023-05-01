d302bbe6d1372db1828171d0e342dd9f0c73e508
Victoria <dmitrieva7v@gmail.com>
2023-05-01 15:58:11 +0300

    Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход трёхзначное число и 
+// 2. на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите целое трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SecondDigit(number);
+Console.WriteLine($"Вторая цифра числа {number} -> {result}");
+
+int SecondDigit(int num)
+{
+    return num % 100 / 10;
+}
