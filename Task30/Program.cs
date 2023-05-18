9171d3ad8b1002d7ccdb850f320bd5385c29516e
Victoria <dmitrieva7v@gmail.com>
2023-05-18 22:35:27 +0300

    Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 30: Напишите программу, которая
+// 1. выводит массив из 8 элементов, 
+// 2. заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] arr = new int[8];
+
+FillArray(arr);
+Console.Write("[");
+PrintArray(arr); 
+Console.Write("]");
+
+void PrintArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    
+}
+
+void FillArray(int[] array)
+{
+    var rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 2);
+    }
+}
+
+// //int[] arr = new int[8];
+
+//            // FillArray(arr);
+//             int[] newArr = CreateArray(12);
+//             Console.Write("[");
+//             PrintArray(newArr);
+//             Console.Write("]");
+
+//             void PrintArray(int[] array)
+//             {
+//                 for (int i = 0; i < array.Length; i++)
+//                 {
+//                     if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+//                     else Console.Write($"{array[i]}");
+//                 }
+
+//             }
+
+//             void FillArray(int[] array)
+//             {
+//                 var rnd = new Random();
+//                 for (int i = 0; i < array.Length; i++)
+//                 {
+//                     array[i] = rnd.Next(0, 2);
+//                 }
+//             }
+// int[] CreateArray(int size)
+//             {
+//                 int[] array = new int[size];
+//                 var rnd = new Random();
+//                 for (int i = 0; i < array.Length; i++)
+//                 {
+//                     array[i] = rnd.Next(0, 2);
+//                 }
+//                 return array;
+//             }
