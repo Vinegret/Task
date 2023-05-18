e54359c4dafc7dbe67c8f193978f8dd25100ead3
Victoria <dmitrieva7v@gmail.com>
2023-05-18 16:05:00 +0300

    Task_20

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -4,27 +4,26 @@
 // 3. находит расстояние между ними 
 // 4. в 2D пространстве 
 
-Console.WriteLine("Введите координаты точки 1 -> ");
-Console.Write("Координата X первой точки: ");
+Console.WriteLine("Введите координаты точки 1: ");
+Console.Write("Координата X первой точки -> ");
 int x1Coordinate = Convert.ToInt32(Console.ReadLine());
-Console.Write("Координата Y первой точки: ");
+Console.Write("Координата Y первой точки -> ");
 int y1Coordinate = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координаты точки 2 -> ");
-Console.Write("Координата X второй точки: ");
+Console.WriteLine("Введите координаты точки 2: ");
+Console.Write("Координата X второй точки -> ");
 int x2Coordinate = Convert.ToInt32(Console.ReadLine());
-Console.Write("Координата Y второй точки: ");
+Console.Write("Координата Y второй точки -> ");
 int y2Coordinate = Convert.ToInt32(Console.ReadLine());
 
 
-int distanse = Distanse(x1Coordinate, y1Coordinate, x2Coordinate, y2Coordinate);
-Console.WriteLine($"Расстояние от точки 1 до точки 2 = {distanse} ");
+double distanse = Distanse(x1Coordinate, y1Coordinate, x2Coordinate, y2Coordinate);
+Console.WriteLine($"Расстояние от точки 1 до точки 2 = {Math.Round(distanse, 2, MidpointRounding.ToZero)}");
 
-int Distanse (int x1, int y1, int x2, int y2)
+double Distanse (int x1, int y1, int x2, int y2)
 {
-    difference1 = (x2 - x1)*(x2 - x1);
-    difference2 = (y2 - y1)*(y2 - y1);
-    num = difference1 + difference2;
-    double square = Math.Sqrt(num);
-    return square;
+    int difference1 = (x2 - x1)*(x2 - x1);
+    int difference2 = (y2 - y1)*(y2 - y1);
+    double hypo = Math.Sqrt(difference1 + difference2);
+    return hypo;
 }
