14bd8c55e76877b7bf0f3337014ab0fd3c035758
Victoria <dmitrieva7v@gmail.com>
2023-06-08 16:45:52 +0300

    Task47 изменено

---
+++ Task47/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrixRndInt(int rows, int columns, double min, double max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next() + rnd.NextDouble();
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int Rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int Columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальное значение");
+double Min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите максимальное значение");
+double Max = Convert.ToInt32(Console.ReadLine());
+
+double[,] array2D = CreateMatrixRndInt(Rows, Columns, Min, Max);
+PrintMatrix(array2D);
